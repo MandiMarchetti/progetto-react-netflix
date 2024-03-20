65123142be16914c7a83dd07b45a20af6fb55aca
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-03-20 14:56:53 +0100

    update

--- src/Components/TrendingNow.jsx
+++ src/Components/TrendingNow.jsx
@@ -8,7 +8,7 @@ import StarWars from "../data/StarWars.json";
 class TrendingNow extends Component {
   render() {
     return (
-      <div className="container-fluid px-4 card_div">
+      <div className="container-fluid card_div">
         <div className="unique_card_div">
           <div className="tvshow_title_div">
             <h4 className="tvshow_title">Trending Now</h4>
@@ -16,7 +16,7 @@ class TrendingNow extends Component {
           <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 trending_div">
             {HarryPotter.slice(0, 6).map((movies) => {
               return (
-                <div className="col mb-2 text-center px-1 movie_card">
+                <div className="col mb-2 text-center movie_card">
                   <img className="img-fluid" src={movies.Poster} alt="movie picture" />
                 </div>
               );
